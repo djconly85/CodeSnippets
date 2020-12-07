84b78de2bbd6bfd9061487899e5c48dcf67276f9
djconly85 <dconly@sacog.org>
2020-12-07 10:04:57 -0800

    Update bcp_loader.py
    minor fixes to script

--- SQL/bulk_loader/bcp_loader.py
+++ SQL/bulk_loader/bcp_loader.py
@@ -20,6 +20,7 @@ Python Version: 3.x
 """
 
 import os
+import sys
 import time
 import subprocess
 
@@ -34,13 +35,13 @@ class BCP():
     def __init__(self, svr_name, db_name, trusted_conn=True):
         
         
-        self.svr_name = svr_name
-        self.db_name = db_name
-        self.db_system ="{SQL Server}" 
+        self.svr_name = svr_name # server name
+        self.db_name = db_name # database name
+        self.db_system ="{SQL Server}" # will always be SQL Server, since BCP is specifically for MSSQL
         
         if trusted_conn:
             self.bcp_auth = '-T' 
-            self.sqla_auth = 'yes' # sql alchemy auth if trusted connection
+            self.conn_auth = 'yes' # pyodbc auth if trusted connection
         else:
             self.username = input("Enter username: ")
             self.password = input("Enter password: ")
@@ -49,8 +50,10 @@ class BCP():
         
         # NOTE 11/21/2020 - SQL ALCHEMY CONNECTION NOT SET UP TO HAVE NON-TRUSTED CONNECTION!
         self.str_conn_info = "Driver={0}; Server={1}; Database={2}; Trusted_Connection={3};" \
-            .format(self.db_system, self.svr_name, self.db_name, self.sqla_auth)
+            .format(self.db_system, self.svr_name, self.db_name, self.conn_auth)
             
+        # dict to ensure correct file delimiter character is used. User specifies
+        # comma or tab type in methods that follow below, not in this __init__ method
         self.delim_char_lookup = {"comma": ',', "tab": '\\t'}
 
         self.use_quoted_identifiers = '-q' #allows loading to table name with spaces in it
@@ -60,7 +63,15 @@ class BCP():
             
     def create_sql_table_from_file(self, file_in, delim_name, str_create_table_sql, tbl_name,
                                    overwrite=True, data_start_row=2):
-        '''Loads data from a text file into a sql server table'''
+        '''Loads data from a text file into a sql server table
+        PARAMETERS:
+            file_in = text or CSV input data file
+            delim_name = name of character used for delimiting
+            str_create_table_sql = string of SQL query, normally read from SQL file
+            tbl_name = name of table to be created
+            overwrite = True/False. If true will overwrite any tables that already exist with tbl_name
+            data_start_row = row that data start on. If data file has header row, then start_row = 2
+        '''
         
         delim_char = self.delim_char_lookup[delim_name]
         
@@ -75,22 +86,21 @@ class BCP():
     
             if tbl_name in tables:
                 if overwrite:
+                    print(f"{tbl_name} already exists. Will be overwritten...")
                     drop_tbl_sql = "DROP TABLE {};".format(tbl_name)
                     sql_cur.execute(drop_tbl_sql)
                 else:
-                    print("{} already exists. Will append to existing table")
-                    # sys.exit()
+                    print(f"{tbl_name} already exists. Exiting script...")
+                    sys.exit()
                 
             # create table that data will load to
             print(f"creating table {tbl_name}...")
             
             sql_cur.execute(str_create_table_sql)
             
-        sql_cur.close()
-        conn.close()
-        
-        
-    
+        # sql_cur.close()
+        # conn.close()
+
         # import pdb; pdb.set_trace()
         # generate bcp command
         loading_dir = 'in' # in = load from file into sql server; out = from server to file
@@ -128,23 +138,28 @@ class BCP():
 
 if __name__ == '__main__':
     
-    #================TEST ZONE======================
+    #================INPUT PARAMETERS======================
     #----------------TEST PARAMS---------------------
-    test_tbl_sql = r"C:\Users\dconly\GitRepos\CodeSnippets\SQL\bulk_loader\create_trip_table_wskimvals.sql"
+    sql_to_run = r"C:\Users\dconly\GitRepos\CodeSnippets\SQL\bulk_loader\SQL test scripts\create_trip_table_wskimvals.sql"
+    
+    # this chunk of params could also just be rewritten as one variable that is a file path
     test_tbl_type = "trip" #household, trip, etc
     run_folder = r'D:\SACSIM19\MTP2020\Conformity_Runs\run_2035_MTIP_Amd1_Baseline_v1'
     txt_file_in = "_trip_1_1.csv"
     delimiter_name = "comma" # acceptable values "comma", "tab"
     test_tbl_name = f"BCPtest_{test_tbl_type}_table"
-    # test_tbl_name = "BCP_hh_TEST"
+    
+    # server and database parameters
+    server_name = 'SQL-SVR'
+    database = 'MTP2020'
         
     #-----------------RUN TEST SCRIPT----------------    
-    with open(test_tbl_sql, 'r') as f_sql_in:
+    with open(sql_to_run, 'r') as f_sql_in:
         raw_sql = f_sql_in.read()
-        formatted_sql = raw_sql.format(test_tbl_name)
+        formatted_sql_str = raw_sql.format(test_tbl_name)
     
-    tbl_loader = BCP(svr_name='SQL-SVR', db_name='MTP2020')
+    tbl_loader = BCP(svr_name=server_name, db_name=database)
     test_file_in = os.path.join(run_folder, txt_file_in)
-    tbl_loader.create_sql_table_from_file(test_file_in, delimiter_name, formatted_sql, test_tbl_name,
+    tbl_loader.create_sql_table_from_file(test_file_in, delimiter_name, formatted_sql_str, test_tbl_name,
                                           overwrite=True)
 
